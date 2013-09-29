e987f148007f6c9d46395370af187ce68d2fb900
qingchengl <qingchengl@opera.com>
2013-09-29 16:57:37 +0800

    mkvendor.sh: Initial commit of jfltezm

--- cm.mk
+++ cm.mk
@@ -1,18 +1,16 @@
-## Specify phone tech before including full_phone
+$(call inherit-product, device/samsung/jfltezm/full_jfltezm.mk)
+
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := jfltezm
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/jfltezm/device_jfltezm.mk)
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltezm TARGET_DEVICE=jfltezm BUILD_FINGERPRINT="samsung/jfltezm/jflte:4.2.2/JDQ39/I9508ZMUAMG1:user/release-keys" PRIVATE_BUILD_DESC="jfltezm-user 4.2.2 JDQ39 I9508ZMUAMG1 release-keys"
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := jfltezm
 PRODUCT_NAME := cm_jfltezm
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := jfltezm
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_DEVICE := jfltezm
+
