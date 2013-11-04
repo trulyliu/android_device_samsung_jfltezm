386b2d01fc9bcb1ba5795516649a3f4cc1931ec6
trulyliu <trulyliu@gmail.com>
2013-11-04 11:22:01 +0800

    remove gsm.mk
    Change-Id: I1f663fb4b205bd83f571dc43132c2d368b6fa9b1

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/jfltezm/full_jfltezm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
